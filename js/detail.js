d0a5204828d2c808f7e2017aca6175f791ac7728
khalid <khalid_munir@yahoo.com>
2019-01-10 02:25:29 +0000

    added first apex chart (code - untidy, requires refactoring)

--- js/detail.js
+++ js/detail.js
@@ -127,3 +127,94 @@ document.getElementById("largest-file").innerHTML = sizes[sizes.length - 1]
                             </tr>
 
 */
+
+// Chart options
+const options = {
+  chart: {
+    height: 450,
+    width: "30%",
+    type: "bar",
+    background: "#f4f4f4",
+    foreColor: "#333"
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false
+    }
+  },
+  series: [
+    {
+      name: "Population",
+      data: [
+        8550405,
+        3971883,
+        2720546,
+        2296224,
+        1567442,
+        1563025,
+        1469845,
+        1394928,
+        1300092,
+        1026908
+      ]
+    }
+  ],
+  xaxis: {
+    categories: [
+      "New York",
+      "Los Angeles",
+      "Chicago",
+      "Houston",
+      "Philadelphia",
+      "Phoenix",
+      "San Antonio",
+      "San Diego",
+      "Dallas",
+      "San Jose"
+    ]
+  },
+  fill: {
+    colors: ["#00ff00", "#ffff00", "#00ffff", "#0000ff", "#ff00ff",        "#00ff00", "#ffff00", "#00ffff", "#0000ff", "#ff00ff"  ]
+  },
+  dataLabels: {
+    enabled: false
+  },
+
+  title: {
+    text: "Largest US Cities By Population",
+    align: "center",
+    margin: 20,
+    offsetY: 20,
+    style: {
+      fontSize: "25px"
+    }
+  }
+};
+
+// Init chart
+const chart = new ApexCharts(document.querySelector("#chart"), options);
+
+// Render chart
+chart.render();
+
+// Event example
+document.querySelector("#change").addEventListener("click", () =>
+  chart.updateOptions({
+    plotOptions: {
+      bar: {
+        horizontal: true
+      }
+    }
+  })
+);
+
+
+document.querySelector("#change2").addEventListener("click", () =>
+  chart.updateOptions({
+    plotOptions: {
+      bar: {
+        horizontal: false
+      }
+    }
+  })
+);
