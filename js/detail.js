d8c47b8225717e8014ce74d1edb2da7514da32ac
khalid <khalid_munir@yahoo.com>
2019-01-11 02:47:24 +0000

    last checkin - added radialBar graph

--- js/detail.js
+++ js/detail.js
@@ -629,7 +629,7 @@ function humanFileSize(bytes, si) {
       chart: {
         id: 'chartyear',
         type: 'area',
-        height: 320,
+        height: 220,
         background: '#F6F8FA',
         toolbar: {
           show: false,
@@ -682,11 +682,11 @@ function humanFileSize(bytes, si) {
       optionsarea2
     );
 
-  //  chartarea2.render();
+    chartarea2.render();
 
     var optionsArea = {
       chart: {
-        height: 400,
+        height: 200,
         type: 'area',
         background: '#F6F8FA',
         toolbar: {
@@ -735,7 +735,7 @@ function humanFileSize(bytes, si) {
       optionsArea
     );
 
- //   chartArea.render();
+    chartArea.render();
     
     
     
@@ -964,3 +964,539 @@ function humanFileSize(bytes, si) {
         );
 
         chartHeat.render();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            window.Apex = {
+      stroke: {
+        width: 3
+      },
+      markers: {
+        size: 0
+      },
+      tooltip: {
+        fixed: {
+          enabled: true,
+        }
+      }
+    };
+    
+    var randomizeArray = function (arg) {
+      var array = arg.slice();
+      var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+      while (0 !== currentIndex) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    }
+
+    // data for the sparklines that appear below header area
+    var sparklineData = [47, 45, 54, 38, 56, 24, 65, 31, 37, 39, 62, 51, 35, 41, 35, 27, 93, 53, 61, 27, 54, 43, 19, 46];
+
+    var spark1 = {
+      chart: {
+        type: 'area',
+        height: 160,
+        sparkline: {
+          enabled: true
+        },
+      },
+      stroke: {
+        curve: 'straight'
+      },
+      fill: {
+        opacity: 0.3,
+        gradient: {
+          enabled: false
+        }
+      },
+      series: [{
+        data: randomizeArray(sparklineData)
+      }],
+      yaxis: {
+        min: 0
+      },
+      colors: ['#DCE6EC'],
+      title: {
+        text: '$424,652',
+        offsetX: 0,
+        style: {
+          fontSize: '24px',
+          cssClass: 'apexcharts-yaxis-title'
+        }
+      },
+      subtitle: {
+        text: 'Sales',
+        offsetX: 0,
+        style: {
+          fontSize: '14px',
+          cssClass: 'apexcharts-yaxis-title'
+        }
+      }
+    }
+
+    var spark2 = {
+      chart: {
+        type: 'area',
+        height: 160,
+        sparkline: {
+          enabled: true
+        },
+      },
+      stroke: {
+        curve: 'straight'
+      },
+      fill: {
+        opacity: 0.3,
+        gradient: {
+          enabled: false
+        }
+      },
+      series: [{
+        data: randomizeArray(sparklineData)
+      }],
+      yaxis: {
+        min: 0
+      },
+      colors: ['#DCE6EC'],
+      title: {
+        text: '$235,312',
+        offsetX: 0,
+        style: {
+          fontSize: '24px',
+          cssClass: 'apexcharts-yaxis-title'
+        }
+      },
+      subtitle: {
+        text: 'Expenses',
+        offsetX: 0,
+        style: {
+          fontSize: '14px',
+          cssClass: 'apexcharts-yaxis-title'
+        }
+      }
+    }
+
+    var spark3 = {
+      chart: {
+        type: 'area',
+        height: 160,
+        sparkline: {
+          enabled: true
+        },
+      },
+      stroke: {
+        curve: 'straight'
+      },
+      fill: {
+        opacity: 0.3,
+        gradient: {
+          enabled: false
+        }
+      },
+      series: [{
+        data: randomizeArray(sparklineData)
+      }],
+      xaxis: {
+        crosshairs: {
+          width: 1
+        },
+      },
+      yaxis: {
+        min: 0
+      },
+      title: {
+        text: '$135,965',
+        offsetX: 0,
+        style: {
+          fontSize: '24px',
+          cssClass: 'apexcharts-yaxis-title'
+        }
+      },
+      subtitle: {
+        text: 'Profits',
+        offsetX: 0,
+        style: {
+          fontSize: '14px',
+          cssClass: 'apexcharts-yaxis-title'
+        }
+      }
+    }
+
+    var spark1 = new ApexCharts(document.querySelector("#spark1"), spark1);
+    spark1.render();
+    var spark2 = new ApexCharts(document.querySelector("#spark2"), spark2);
+    spark2.render();
+    var spark3 = new ApexCharts(document.querySelector("#spark3"), spark3);
+    spark3.render();
+
+    var options1 = {
+      chart: {
+        type: 'line',
+        width: 100,
+        height: 35,
+        sparkline: {
+          enabled: true
+        }
+      },
+      series: [{
+        data: [25, 66, 41, 89, 63, 25, 44, 12, 36, 9, 54]
+      }],
+      tooltip: {
+        fixed: {
+          enabled: false
+        },
+        x: {
+          show: false
+        },
+        y: {
+          title: {
+            formatter: function (seriesName) {
+              return ''
+            }
+          }
+        },
+        marker: {
+          show: false
+        }
+      }
+    }
+
+    var options2 = {
+      chart: {
+        type: 'line',
+        width: 100,
+        height: 35,
+        sparkline: {
+          enabled: true
+        }
+      },
+      series: [{
+        data: [12, 14, 2, 47, 42, 15, 47, 75, 65, 19, 14]
+      }],
+      tooltip: {
+        fixed: {
+          enabled: false
+        },
+        x: {
+          show: false
+        },
+        y: {
+          title: {
+            formatter: function (seriesName) {
+              return ''
+            }
+          }
+        },
+        marker: {
+          show: false
+        }
+      }
+    }
+
+    var options3 = {
+      chart: {
+        type: 'line',
+        width: 100,
+        height: 35,
+        sparkline: {
+          enabled: true
+        }
+      },
+      series: [{
+        data: [47, 45, 74, 14, 56, 74, 14, 11, 7, 39, 82]
+      }],
+      tooltip: {
+        fixed: {
+          enabled: false
+        },
+        x: {
+          show: false
+        },
+        y: {
+          title: {
+            formatter: function (seriesName) {
+              return ''
+            }
+          }
+        },
+        marker: {
+          show: false
+        }
+      }
+    }
+
+    var options4 = {
+      chart: {
+        type: 'line',
+        width: 100,
+        height: 35,
+        sparkline: {
+          enabled: true
+        }
+      },
+      series: [{
+        data: [15, 75, 47, 65, 14, 2, 41, 54, 4, 27, 15]
+      }],
+      tooltip: {
+        fixed: {
+          enabled: false
+        },
+        x: {
+          show: false
+        },
+        y: {
+          title: {
+            formatter: function (seriesName) {
+              return ''
+            }
+          }
+        },
+        marker: {
+          show: false
+        }
+      }
+    }
+
+    var options5 = {
+      chart: {
+        type: 'bar',
+        width: 100,
+        height: 35,
+        sparkline: {
+          enabled: true
+        }
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: '80%'
+        }
+      },
+      series: [{
+        data: [25, 66, 41, 89, 63, 25, 44, 12, 36, 9, 54]
+      }],
+      labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      xaxis: {
+        crosshairs: {
+          width: 1
+        },
+      },
+      tooltip: {
+        fixed: {
+          enabled: false
+        },
+        x: {
+          show: false
+        },
+        y: {
+          title: {
+            formatter: function (seriesName) {
+              return ''
+            }
+          }
+        },
+        marker: {
+          show: false
+        }
+      }
+    }
+
+    var options6 = {
+      chart: {
+        type: 'bar',
+        width: 100,
+        height: 35,
+        sparkline: {
+          enabled: true
+        }
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: '80%'
+        }
+      },
+      series: [{
+        data: [12, 14, 2, 47, 42, 15, 47, 75, 65, 19, 14]
+      }],
+      labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      xaxis: {
+        crosshairs: {
+          width: 1
+        },
+      },
+      tooltip: {
+        fixed: {
+          enabled: false
+        },
+        x: {
+          show: false
+        },
+        y: {
+          title: {
+            formatter: function (seriesName) {
+              return ''
+            }
+          }
+        },
+        marker: {
+          show: false
+        }
+      }
+    }
+
+    var options7 = {
+      chart: {
+        type: 'bar',
+        width: 100,
+        height: 35,
+        sparkline: {
+          enabled: true
+        }
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: '80%'
+        }
+      },
+      series: [{
+        data: [47, 45, 74, 14, 56, 74, 14, 11, 7, 39, 82]
+      }],
+      labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      xaxis: {
+        crosshairs: {
+          width: 1
+        },
+      },
+      tooltip: {
+        fixed: {
+          enabled: false
+        },
+        x: {
+          show: false
+        },
+        y: {
+          title: {
+            formatter: function (seriesName) {
+              return ''
+            }
+          }
+        },
+        marker: {
+          show: false
+        }
+      }
+    }
+
+    var options8 = {
+      chart: {
+        type: 'bar',
+        width: 100,
+        height: 35,
+        sparkline: {
+          enabled: true
+        }
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: '80%'
+        }
+      },
+      series: [{
+        data: [25, 66, 41, 89, 63, 25, 44, 12, 36, 9, 54]
+      }],
+      labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      xaxis: {
+        crosshairs: {
+          width: 1
+        },
+      },
+      tooltip: {
+        fixed: {
+          enabled: false
+        },
+        x: {
+          show: false
+        },
+        y: {
+          title: {
+            formatter: function (seriesName) {
+              return ''
+            }
+          }
+        },
+        marker: {
+          show: false
+        }
+      }
+    }
+
+    new ApexCharts(document.querySelector("#schart1"), options1).render();
+    new ApexCharts(document.querySelector("#schart2"), options2).render();
+    new ApexCharts(document.querySelector("#schart3"), options3).render();
+    new ApexCharts(document.querySelector("#schart4"), options4).render();
+    new ApexCharts(document.querySelector("#schart5"), options5).render();
+    new ApexCharts(document.querySelector("#schart6"), options6).render();
+    new ApexCharts(document.querySelector("#schart7"), options7).render();
+    new ApexCharts(document.querySelector("#schart8"), options8).render();
+  
+        
+        
+        
+        
+        
+        
+        
+  var optionsRadialBar = {
+            chart: {
+                height: 350,
+                type: 'radialBar',
+            },
+            plotOptions: {
+                radialBar: {
+                    dataLabels: {
+                        name: {
+                            fontSize: '22px',
+                        },
+                        value: {
+                            fontSize: '16px',
+                        },
+                        total: {
+                            show: true,
+                            label: 'Total',
+                            formatter: function (w) {
+                                // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
+                                return 249
+                            }
+                        }
+                    }
+                }
+            },
+            series: [44, 55, 67, 83],
+            labels: ['Apples', 'Oranges', 'Bananas', 'Berries'],
+            
+        }
+
+       var chartRadialBar = new ApexCharts(
+            document.querySelector("#chartRadialBar"),
+            optionsRadialBar
+        );
+        
+        chartRadialBar.render();
+        
+        
